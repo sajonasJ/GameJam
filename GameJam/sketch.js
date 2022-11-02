c52f164bd00f81cb8e5570ffeac821da37b17334
Jonas <jonas.sajonas@gmail.com>
2022-11-02 20:20:37 +1000

    added returnbutton and enhanced readability

--- GameJam/sketch.js
+++ GameJam/sketch.js
@@ -1,12 +1,15 @@
 "use strict"
-let squareSprite;
-let playButton, creditsButton, settingsButton;
+////////////////////////VARIABLES////////////////////////
+let playButton, creditsButton, settingsButton,returnButton;
+
+////////////////////////IMAGE ANIMATION VARIABLES////////////////////////
 var runningSpriteSheet;
 var runningAnim;
 var idleSpriteSheet;
 var idleAnim;
 var runningLeftSpriteSheet;
 var runningLeftAnim;
+let squareSprite;
 
 function preload(){
   runningSpriteSheet = loadSpriteSheet("assets/images/running1400.png",100, 100, 6)
@@ -15,7 +18,6 @@ function preload(){
   runningLeftAnim = loadAnimation(runningLeftSpriteSheet)
   idleSpriteSheet = loadSpriteSheet("assets/images/idle1400.png", 100, 100, 7)
   idleAnim = loadAnimation(idleSpriteSheet)
-  
 }
 
 function setup() {
@@ -31,43 +33,35 @@ function setup() {
 
 function draw() {
   drawScreens();
-  
 }
-
-function screenChanger(){
-  if (49){
-
-  }
-}
-
-
-
+////////////////////////DRAWSCREEN CONTROL////////////////////////
 function drawMainMenu() {
   background(155);
-  playButton.show()
-  creditsButton.show()
-  settingsButton.show()
+  showMainMenuButtons();
+  returnButton.hide();
   creditsButton.mousePressed(viewCredits);
   playButton.mousePressed(viewGame);
   settingsButton.mousePressed(viewSettings);
-  
-
+  returnButton.mousePressed(viewMainMenu);
 }
 
 function drawGamePlay() {
   hideMainMenuButtons();
+  returnButton.hide();
+
+
+  camera.position.x = squareSprite.position.x;//CAMERA CONTROL X
+  camera.position.y = squareSprite.position.y;//CAMERA CONTROL Y
+
+  ////////////////////////TEMP BCKGRND////////////////////////
+  let edge = 1280;
+  
   background(0);
   keyPressed();
   fill("red");
-  rect(1800, 300, 50, 50);
-
+  rect(1800, 300, 50, 50);//box
   fill("red");
-  rect(4000, 300, 50, 50);
-
-  camera.position.x = squareSprite.position.x;
-  camera.position.y = squareSprite.position.y;
-
-  let edge = 1280;
+  rect(4000, 300, 50, 50);//box
   fill('green');
   rect(0, 360, edge, 360);//stage1
   fill('gray');
@@ -76,10 +70,28 @@ function drawGamePlay() {
   rect(edge * 2, 360, edge, 360);//stage3
   fill('lightblue');
   rect(0, 0, edge * 3, 360);//stage3
-
+  ////////////////////////TEMP BCKGRND////////////////////////
+ 
   drawSprites();
 }
 
+function drawSettings() {
+  background("yellow");
+  hideMainMenuButtons();
+  returnButton.show();
+}
+function drawCredits() {
+  background("green");
+  hideMainMenuButtons();
+  returnButton.show();
+}
+function drawLose() {
+  background(155);
+}
+function drawWin() {
+  background(155);
+}
+////////////////////////CONTROL BUTTONS////////////////////////
 function keyPressed() {
   let up = keyDown(UP_ARROW);
   let down = keyDown(DOWN_ARROW);
@@ -104,24 +116,32 @@ function keyPressed() {
     squareSprite.changeAnimation("idle")
   }
 }
-
-
-function drawSettings() {
-  background("yellow");
-  hideMainMenuButtons();
+function keyPressed() {
+  let up = keyDown(UP_ARROW);
+  let down = keyDown(DOWN_ARROW);
+  let left = keyDown(LEFT_ARROW);
+  let right = keyDown(RIGHT_ARROW);
+  let w = keyDown('w');
+  let a = keyDown('a');
+  let s = keyDown('s');
+  let d = keyDown('d');
+//LOGIC
+  if (up || w) {
+    squareSprite.velocity.y = -3;
+  }else if (left || a) {
+    squareSprite.velocity.x = -5;
+    squareSprite.changeAnimation("runningLeft")
+  }else if (right || d) {
+    squareSprite.velocity.x = 5;
+    squareSprite.changeAnimation("running")
+  }else if (down || s) {
+    squareSprite.velocity.y = 3;
+  }else{
+    squareSprite.changeAnimation("idle")
+  }
 }
-function drawCredits() {
-  background("green");
-  hideMainMenuButtons();
-}
 
-function drawLose() {
-  background(155);
-}
-function drawWin() {
-  background(155);
-}
-
+////////////////////////CREATE BUTTONS////////////////////////
 function buttonManager(){
     playButton = createButton("PLAY")
     playButton.position(width/2 -50, 300)
@@ -135,11 +155,23 @@ function buttonManager(){
     settingsButton.position(width/2 -50, 500)
     settingsButton.size(100,50)
     settingsButton.hide()
-    
+    returnButton = createButton("RETURN")
+    returnButton.position(width/2 -50, 500)
+    returnButton.size(100,50)
+    returnButton.hide()
 }
 
-
-
+function showMainMenuButtons(){
+  playButton.show()
+  creditsButton.show()
+  settingsButton.show()
+}
+function hideMainMenuButtons(){
+  playButton.hide()
+  creditsButton.hide()
+  settingsButton.hide()
+}
+////////////////////////CURRENTSTATE CONTROL////////////////////////
 function viewCredits(){
   currentState = CREDITS
 }
@@ -149,9 +181,6 @@ function viewGame(){
 function viewSettings(){
   currentState = SETTINGS
 }
-
-function hideMainMenuButtons(){
-  playButton.hide()
-  creditsButton.hide()
-  settingsButton.hide()
+function viewMainMenu(){
+  currentState = MAIN_MENU
 }
