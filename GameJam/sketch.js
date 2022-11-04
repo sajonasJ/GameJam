66a0dae6c3b49fed97b7485b4f84d5874172a9d9
Ryotankobu <ucp4fya7@yahoo.co.jp>
2022-11-04 17:33:29 +1000

    changes

--- GameJam/sketch.js
+++ GameJam/sketch.js
@@ -11,6 +11,11 @@ var runningLeftSpriteSheet;
 var runningLeftAnim;
 let player;
 let background1;
+let settingBackground;
+let returnButtonIMG;
+let volumeSlider;
+let levelSlider;
+let creditBackground;
 
 function preload(){
   runningSpriteSheet = loadSpriteSheet("assets/images/running1400.png",100, 100, 6)
@@ -27,6 +32,9 @@ function preload(){
   mainMenuBG = loadImage("assets/images/mainMenuBG.png")
   playButtonIMG = loadImage("assets/images/playbutton.png.png")
   creditsButtonIMG = loadImage("assets/images/creditsButton.png")
+  settingBackground = loadImage("assets/images/settingbackground.png")
+  returnButtonIMG = loadImage("assets/images/returnButtonIMG.png")
+  creditBackground = loadImage("assets/images/creditbackground.jpg")
 }
 
 function setup() {
@@ -38,6 +46,7 @@ function setup() {
   player.addAnimation("runningLeft", runningLeftAnim)
   player.friction = 0.15;
   buttonManager();
+  sliderManager();
 }
 
 function draw() {
@@ -53,6 +62,8 @@ function drawMainMenu() {
   playButton.mousePressed(viewGame);
   settingsButton.mousePressed(viewSettings);
   returnButton.mousePressed(viewMainMenu);
+  volumeSlider.hide();
+  levelSlider.hide();
 }
 
 function drawGamePlay() {
@@ -73,12 +84,40 @@ camera.zoom = 1.5
 }
 
 function drawSettings() {
-  background("yellow");
+  //background("yellow");
+  image(settingBackground,0,0)
+  noStroke();
+  fill("white")
+  rect(220,150,300,150,30);
+  rect(720,150,400,150,30);
+  fill("gray");
+  textSize(30);
+  textFont(font);
+  text("volume",300,210);
+  text("difficulty",800,210);
+  textSize(15);
+  text("low", 260,280);
+  text("high", 430,280);
+  text("easy", 740,280);
+  text("normal", 890,280);
+  text("hard", 1060,280);
   hideMainMenuButtons();
   returnButton.show();
+  volumeSlider.show();
+  levelSlider.show();
+  
+
+  
+
+
+  
 }
 function drawCredits() {
-  background("green");
+  image(creditBackground,0,0);
+  fill(255, 200);
+  noStroke();
+  rect(200,100,880,550,30);
+  
   hideMainMenuButtons();
   returnButton.show();
 }
@@ -119,10 +158,11 @@ function buttonManager(){
     creditsButton = createImg("assets/images/creditsButton.png")
     creditsButton.position(950,150)
     
-    returnButton = createButton("RETURN")
-    returnButton.position(width/2 -50, 500)
-    returnButton.size(100,50)
+    returnButton = createImg("assets/images/returnButtonIMG.png")
+    returnButton.position(1100, 50)
+    returnButton.size(80,80)
     returnButton.hide()
+    
 
     settingsButton = createImg("assets/images/settingsCog.png")
     settingsButton.position(50,50)
@@ -130,6 +170,22 @@ function buttonManager(){
     playButton = createImg("assets/images/playButton.png.png")
     playButton.position(950, 50)
     
+}
+
+function sliderManager(){
+ 
+
+  volumeSlider = createSlider(0, 100, 0);
+  volumeSlider.position(270, 230);
+  volumeSlider.style("width", "200px");
+  //volumeSlider.mousePressed(sliderPressed)
+
+  levelSlider = createSlider(0, 2, 0);
+  levelSlider.position(750, 230);
+  levelSlider.style("width", "350px");
+  //volumeSlider.mousePressed(sliderPressed)
+  
+
 }
 
 function showMainMenuButtons(){
